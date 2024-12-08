eccd16d69334fdff2ebcec35e066b4e53461cbbe
ZakariaBj <zakariabenjeddi22@gmail.com>
2024-12-08 13:33:03 +0100

    insert depuis python to mongodb collection by insert_one et non insertOne

--- mongodb.py
+++ mongodb.py
@@ -35,17 +35,30 @@ PythonMongodb = myclient["PythonMongodb"]
 
 client_collection = PythonMongodb['client']
 
+# TODO initialisation des key de document
+# id = int(input("enterer un id : "))
+# prenom = (input("entrer le prenom de client : ")) 
+# tel = (input("entrer le tel de client : ")) 
+# adress_laivraison = (input("entrer le adress_laivraison de client : "))
+
+# print("id de client : ", id)
+# print("prenom de client : ", prenom)
+# print("tel de client : ", tel)
+# print("adress laivresson de client : ", adress_laivraison)
+
+# client_collection.insert_one({'_id':id,'prenom':prenom,'tel':tel,'adress_laivraison':adress_laivraison})
+
 allClient = client_collection.find()
+
 for client in allClient:
-  print(client)
+  id = client.get('_id', "Inconnu")
+  prenom = client.get('prenom', "Inconnu")
+  tel = client.get('tel', "Inconnu")
+  adress_laivraison = client.get('adress_laivraison', "Inconnu")
 
-
-
-
-
-
-
-
-
-
+  print(id)
+  print(prenom)
+  print(tel)
+  print(adress_laivraison)
+  print("-"*30)
 
